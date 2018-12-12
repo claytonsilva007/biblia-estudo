5a3c145a954f3ff70cc4326ea61fe9c01d9a08fc
claytonsilva007 <claytonsilva007@gmail.com>
2018-12-12 21:43:15 -0200

    conclusão do esqueleto principal da devocional diária. Pendente a inclusão de todos os versículos.

--- src/pages/devocional-diaria/devocional-diaria.ts
+++ src/pages/devocional-diaria/devocional-diaria.ts
@@ -11,10 +11,13 @@ export class DevocionalDiariaPage {
 
   devocionais: Devocional[];
   devocionalDoDia: Devocional;
+  dataAtual: string;
+  data: Date;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.devocionais = this.povoarDevocionais();
     this.getDevocionalDoDia();
+    this.dataAtual = this.dataAtualFormatada();
   }
 
   getDevocionalDoDia(){
@@ -25,6 +28,16 @@ export class DevocionalDiariaPage {
     let devocionais: Devocional[] = [];
     devocionais.push(new Devocional("Adoração a Deus", "E far-te-ei uma grande nação, e abençoar-te-ei e engrandecerei o teu nome; e tu serás uma bênção. E abençoarei os que te abençoarem, e amaldiçoarei os que te amaldiçoarem; e em ti serão benditas todas as famílias da terra.", "Gênesis 12.2-3"));
     return devocionais;
+  }
+
+  dataAtualFormatada(){
+    var data = new Date(),
+        dia  = data.getDate().toString(),
+        diaF = (dia.length == 1) ? '0'+dia : dia,
+        mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+        mesF = (mes.length == 1) ? '0'+mes : mes,
+        anoF = data.getFullYear();
+    return diaF+"/"+mesF+"/"+anoF;
   }
   
 }
