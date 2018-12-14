eb39eee8453bfc10a386ccbf0748352c529d8608
claytonsilva007 <claytonsilva007@gmail.com>
2018-12-14 15:52:04 -0200

    conclusão da tela fale conosco.

--- src/pages/fale-conosco/fale-conosco.ts
+++ src/pages/fale-conosco/fale-conosco.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the FaleConoscoPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, ToastController } from 'ionic-angular';
+import { SocialSharing } from '@ionic-native/social-sharing';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -15,11 +10,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FaleConoscoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  credentialsForm: FormGroup;
+  erro: string;
+  
+  constructor(public navCtrl: NavController, private socialSharing: SocialSharing, private toastCtrl: ToastController, private formBuilder: FormBuilder) {
+    
+    this.erro = "";
+
+    this.credentialsForm = this.formBuilder.group({
+      nome: ['', [Validators.required]]
+    });
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FaleConoscoPage');
+  enviarEmail(){
+
+    this.socialSharing.canShareViaEmail().then(() => {
+      
+      this.socialSharing.shareViaEmail('Agradecemos o contato - ' + this.credentialsForm.controls['nome'].value, 
+          'Feedback - ' + this.credentialsForm.controls['nome'].value, 
+          ['bibliadeestudocolaborativo@gmail.com']).then(() => {
+
+        this.presentToast("Obrigado por manter contato conosco. Seu email foi enviado com sucesso");
+
+      }).catch((e) => {
+        this.presentToast("Erro ao tentar enviar email. " +  e);
+      });
+    }).catch((e) => {
+      this.presentToast("Erro ao tentar enviar email. " +  e);
+    });
   }
 
+  presentToast(msg: string) {
+    const toast = this.toastCtrl.create({
+      message: msg,
+      duration: 5000
+    });
+    toast.present();
+  }
 }
